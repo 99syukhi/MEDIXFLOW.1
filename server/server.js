b74446bdbf43fbab875c406764bf99f27f37482f
Melly Amat <jomellyamatbahrowi@gmail.com>
2026-03-18 23:47:59 -0600

    the official full stack

--- server/server.js
+++ server/server.js
@@ -1,21 +1,27 @@
 const express = require('express');
-const cors = require('cors');
+const path = require('path');
 const app = express();
 
-const authRoutes = require('./routes/authRoutes'); 
+const authRoutes = require('./routes/authRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const patientRoutes = require('./routes/patientRoutes');
 
 const PORT = 5000;
 
-app.use(cors({ origin: 'http://127.0.0.1:5500', credentials: true }));
 app.use(express.json());
 
+// API routes
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/user', patientRoutes);
 
 app.get('/api/status', (req, res) => res.json({ status: "ok", message: "MedixFlow Backend draait!" }));
+
+app.use(express.static(path.join(__dirname, '..', 'client')));
+
+app.get('/{*path}', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`🚀 Server draait op http://localhost:${PORT}`);
